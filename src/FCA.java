8cc2250df93ef4eec2d829b60bcdb39ae0591987
Pankaj Kumar <pankajxdx@gmail.com>
2014-04-21 17:59:44 +0530

    Add remove closed nodes and condense nodes

--- src/FCA.java
+++ src/FCA.java
@@ -9,13 +9,15 @@ public class FCA {
 	static String tmpS;
 	static int[][] adjMat;
 	static Integer tmpI, numObj, numAttr;
+	
 	static HashMap<String, ArrayList<String>> dict = new HashMap<>();
+	
 	static ArrayList<Integer> hasSuccesor = new ArrayList<Integer>();
 	static ArrayList<Integer> hasPredecessor= new ArrayList<Integer>();
 	
 	static ArrayList<String> obj = new ArrayList<String>();
 	static ArrayList<String> attr = new ArrayList<String>();
-	static ArrayList<Pair<ArrayList<String>, ArrayList<String>>> bpc;
+	static BPCliques bpc;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -52,18 +54,60 @@ public class FCA {
 				adjMat[i][j]=tmpI;
 			}
 		}
+//		System.out.println("Printing matrix");
+//		
+//		for(int i=0; i<numObj; i++){
+//			for(int j=0; j<numAttr; j++){
+//				System.out.print(adjMat[i][j]);
+//			}
+//			System.out.println();
+//			
+//		}
 		
 		bpc = getBPClique(adjMat);
 		System.out.println(bpc.size());
 		
+
 		
-//		for(int i=0; i<obj.size(); i++){
-//			System.out.println(obj.get(i));
-//		}
-//		
-//		System.out.println(tmpS);
+		int size1, size2;
+		size1 = bpc.size();
+		size2 = -1;
+		while(size1 != size2){
+			size1 = bpc.size();
+			bpc = condenseList(bpc);
+			size2 = bpc.size();
+		}
+		
+		bpc = removeUnclosed(bpc);
+		
+		//////////////////////////////////////////////////////
+		System.out.println("Printing elements");
+		for(int i =0; i< bpc.size(); i++){
+			printArrayList(bpc.get(i).getL());
+			printArrayList(bpc.get(i).getR());
+			System.out.println();
+		}
+		//////////////////////////////////////////////////////
+
 		
 		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void printArrayList (ArrayList inputList){
+		for(int i=0; i<inputList.size(); i++){
+			System.out.print(inputList.get(i));
+			
+		}
+		System.out.println();
 	}
 	
 	public static BPCliques getBPClique(int[][] mat){
@@ -85,10 +129,15 @@ public class FCA {
 				}
 			}
 			
+//			System.out.print("What i'm pushing: ");
+//			printArrayList(tmpObj);
+//			printArrayList(tmpList);
+			
 			tmpPair = new Pair<ArrayList<String>, ArrayList<String>>(tmpObj, tmpList);
 			tmpBpc.add(tmpPair);
 			dict.put(obj.get(i), tmpList);
 		}
+		
 		
 		for(int i=0; i<numAttr; i++){
 			tmpList = new ArrayList<String>();
@@ -96,7 +145,7 @@ public class FCA {
 			tmpAttr.add(attr.get(i));
 			
 			for(int j=0; j<numObj; j++){
-				if(adjMat[i][j]==1){
+				if(adjMat[j][i]==1){
 					tmpList.add(obj.get(j));
 				}
 			}
@@ -111,8 +160,11 @@ public class FCA {
 	
 	public static BPCliques condenseList(BPCliques inputList){
 		
-		BPCliques tmpList = new BPCliques();
+		BPCliques tmpBCList = new BPCliques();
 		ArrayList<Integer> toSkip = new ArrayList<Integer>();
+		ArrayList<String> tmpAList;
+		HashSet<String> tmpHSet;
+		Pair<ArrayList<String>, ArrayList<String>> tmpPair;
 		Boolean matched;
 		
 		for(int i=0; i<inputList.size(); i++){
@@ -122,14 +174,103 @@ public class FCA {
 			for(int j=i+1; j<inputList.size(); j++){
 				if(toSkip.contains(j)) continue;
 				
-				if(new HashSet<String>(inputList.get(i).getL()).equals(new HashSet<String>(inputList.get(j).getL()))){
+				if(new HashSet<String>(inputList.get(i).getL()).equals( new HashSet<String>(inputList.get(j).getL()) )){
+					tmpHSet = new HashSet<String>(inputList.get(i).getR());
+					tmpHSet.addAll(new HashSet<String>(inputList.get(j).getR()));
+					tmpAList = new ArrayList<>(tmpHSet);
+					tmpPair = new Pair<ArrayList<String>, ArrayList<String>>(inputList.get(i).getL(), tmpAList);
 					
+					tmpBCList.add(tmpPair);
+					toSkip.add(j);
+					matched = true;
+					break;
+				}
+				else if(new HashSet<String>(inputList.get(i).getR()).equals( new HashSet<String>(inputList.get(j).getR()) )){
+					tmpHSet = new HashSet<String>(inputList.get(i).getL());
+					tmpHSet.addAll(new HashSet<String>(inputList.get(j).getL()));
+					tmpAList = new ArrayList<>(tmpHSet);
+					tmpPair = new Pair<ArrayList<String>, ArrayList<String>>(tmpAList, inputList.get(i).getR());
+					
+					tmpBCList.add(tmpPair);
+					toSkip.add(j);
+					matched = true;
+					break;
 				}
 				
 			}
+			if (matched == false){
+				tmpBCList.add(inputList.get(i));
+			}
 		}
 		
-		return tmpList;
+		return tmpBCList;
+		
+	}
+	
+	public static BPCliques removeUnclosed(BPCliques inputList){
+		BPCliques tmpBCList = new BPCliques();
+		ArrayList<String> listO;
+		ArrayList<String> listA;
+		HashSet<String> tmpH;
+		HashSet<String> tmpO;
+		HashSet<String> tmpA;
+		
+//		for(int i=0; i<inputList.size(); i++){
+//			listO = new ArrayList<String>();
+//			listA = new ArrayList<String>();
+//			
+//			for(int j=0; j<inputList.get(i).getL().size(); j++){
+//				if(listA.size() == 0){
+//					listA = new ArrayList<String>(dict.get(inputList.get(i).getL().get(j)));
+//				}else{
+//					tmpH = new HashSet<String>(listA);
+//					tmpH.retainAll(new HashSet<String>(dict.get(inputList.get(i).getL().get(j))));
+//					listA = new ArrayList<String>(tmpH);
+//					
+//				}
+//				
+//			}
+//			
+//			for(int j=0; j<inputList.get(i).getR().size(); j++){
+//				if(listO.size() == 0){
+//					listO = new ArrayList<String>(dict.get(inputList.get(i).getR().get(j)));
+//				}else{
+//					tmpH = new HashSet<String>(listA);
+//					tmpH.retainAll(new HashSet<String>(dict.get(inputList.get(i).getR().get(j))));
+//					listA = new ArrayList<String>(tmpH);
+//					
+//				}
+//				
+//			}
+		
+		
+		
+		for(int i=0; i<inputList.size(); i++){
+			tmpO = new HashSet<String>();
+			tmpA = new HashSet<String>();
+			
+			for(int j=0; j<inputList.get(i).getL().size(); j++){
+				if(tmpA.size() == 0) tmpA = new HashSet<String>(dict.get(inputList.get(i).getL().get(j)));
+				else{
+					tmpA.retainAll(new HashSet<String>(dict.get(inputList.get(i).getL().get(j))));
+				}
+			}
+			
+			
+			for(int j=0; j<inputList.get(i).getR().size(); j++){
+				if(tmpO.size() == 0) tmpO = new HashSet<String>(dict.get(inputList.get(i).getR().get(j)));
+				else{
+					tmpO.retainAll(new HashSet<String>(dict.get(inputList.get(i).getR().get(j))));
+				}
+			}
+			
+			
+			if (tmpA.equals(new HashSet<String>(inputList.get(i).getR())) && tmpO.equals(new HashSet<String>(inputList.get(i).getL()))){
+				tmpBCList.add(inputList.get(i));
+			}
+			
+		}
+		return tmpBCList;
 		
 	}
 
